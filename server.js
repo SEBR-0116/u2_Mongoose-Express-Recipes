55033d72d27b0c4dac471a81bb48660c9071b311
AbeGue02 <abrahamgue02@gmail.com>
2024-02-12 12:28:43 -0500

    Adds server.js functionality for all CRUD ops

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const db = require('./db');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { getCuisineTypes, getCuisineTypeById } = require('./controllers/cuisineTypeController');
-const { getDirections, getDirectionById } = require('./controllers/directionController');
-const { getIngredients, getIngredientById } = require('./controllers/ingredientController');
-const { getRecipes, getRecipeById } = require('./controllers/recipeController');
+const { getCuisineTypes, getCuisineTypeById, updateCuisineType, deleteCuisineType, createCuisineType } = require('./controllers/cuisineTypeController');
+const { getDirections, getDirectionById, updateDirection, deleteDirection, createDirection } = require('./controllers/directionController');
+const { getIngredients, getIngredientById, createIngredient, updateIngredient, deleteIngredient } = require('./controllers/ingredientController');
+const { getRecipes, getRecipeById, createRecipe, updateRecipe, deleteRecipe } = require('./controllers/recipeController');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -17,18 +17,34 @@ app.use(logger('dev'))
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
-app.get('/', (req,res) => {
+app.get('/', async (req,res) => {
     res.send("Welcome to my Cookbook!")
 })
 
 app.get('/recipes', getRecipes)
+app.post('/recipes/create', createRecipe)
 app.get('/recipes/:id', getRecipeById)
+app.put('/recipes/:id/update', updateRecipe)
+app.delete('/recipes/:id/delete', deleteRecipe)
 
 app.get('/ingredients', getIngredients)
+app.post('/ingredients/create', createIngredient)
 app.get('/ingredients/:id', getIngredientById)
+app.put('/ingredients/:id/update', updateIngredient)
+app.delete('/ingredients/:id/delete', deleteIngredient)
 
 app.get('/cuisines', getCuisineTypes)
+app.post('/cuisines/create', createCuisineType)
 app.get('/cuisines/:id', getCuisineTypeById)
+app.put('/cuisines/:id/update', updateCuisineType)
+app.delete('/cuisines/:id/delete', deleteCuisineType)
 
 app.get('/directions', getDirections)
+app.post('/directions/create', createDirection)
 app.get('/directions/:id', getDirectionById)
+app.put('/directions/:id/update', updateDirection)
+app.delete('/directions/:id/delete', deleteDirection)
+
+app.get('/*', async (req,res) => {
+    res.send('An error has occurred. Try again later (404)')
+})
