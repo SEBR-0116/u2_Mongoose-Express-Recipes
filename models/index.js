0cd934f9c68182317bd95db9e4d3a33838593a39
Denesh Anandathasan <denesh555@gmail.com>
2024-02-12 23:13:56 -0500

    direction seeded

--- models/index.js
+++ models/index.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose')
 const cousinTypeSchema = require('./cousintype')
 const recipesSchema = require('./recipe')
 const ingredientSchema = require('./ingredient')
-// const directionSchema = require('./direction')
+const directionSchema = require('./direction')
 
 
 const CousinType = mongoose.model('cousintype',cousinTypeSchema)
 const Recipe = mongoose.model('recipe',recipesSchema)
 const Ingredient = mongoose.model('ingredient',ingredientSchema)
-// const Direction = mongoose.model('direction',directionSchema)
+const Direction = mongoose.model('direction',directionSchema)
 
 
 module.exports = {
 
   CousinType,
   Recipe,
-  Ingredient
-  // Direction
+  Ingredient,
+  Direction
 
 }
